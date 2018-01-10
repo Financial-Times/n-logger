92f772653bd7169073cbe61d64a696f98a2934d4
Zhenyu Lin <zhenyu.lyn@gmail.com>
2018-01-10 16:06:16 +0000

    update  🐿 v2.6.0

--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,4 @@
 require('babel-register')({
 	// just transpile the tests
-	ignore: filename => {
-		/* eslint-disable no-console */
-		console.log(filename);
-		console.log(!(/n-logger\/test/.test(filename)));
-		return !(/n-logger\/test/.test(filename));
-	}
+	ignore: filename => !(/test/.test(filename))
 });
