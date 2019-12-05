a442bb117c8c8114dfb601888a07bbcb2afaa09c
Alex Muller <alex.muller@ft.com>
2019-12-05 16:44:55 +0000

    Allow overriding Splunk host field
    When integrating n-logger with some apps that run on FT Platform,
    I'd like to send the hostname of the machine - we have this information
    currently in Splunk and I'd like to maintain backwards compatibility
    with our existing logs. 🐿 v2.12.5

--- src/lib/transports/splunkHEC.js
+++ src/lib/transports/splunkHEC.js
@@ -25,7 +25,7 @@ class SplunkHEC extends winston.Transport {
 
 		const data = {
 			'time': Date.now(),
-			'host': 'localhost',
+			'host': process.env.N_LOGGER_HOSTNAME || 'localhost',
 			'source': `/var/log/apps/${hostPlatform}/ft-${process.env.SYSTEM_CODE}.log`,
 			'sourcetype': process.env.SPLUNK_SOURCETYPE || '_json',
 			'index': process.env.SPLUNK_INDEX || 'heroku',
